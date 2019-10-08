c7f79478c1dae790cd5f5d80a1cc881bbe9458d4
Jordan McKenzie <jordanmckenz@gmail.com>
2019-10-08 14:05:56 -0700

    added functionality for viewBox to use object as pointer. Doesn't work with canvas yet

--- get-squares/script.js
+++ get-squares/script.js
@@ -1,4 +1,64 @@
-//scripts
-for(var x=0; x<5; x++){
-    console.log(x);
+
+
+changeCursor = function(e){
+    var x = e.clientX;
+    var y = e.clientY;
+    var coor = "Coordinates: (" + x + "," + y + ")";
+    document.getElementById("demo").innerHTML = coor; 
+}
+
+function makeDraggable(evt) {
+    var svg = evt.target;
+
+    svg.addEventListener('mousedown', startDrag);
+    svg.addEventListener('mousemove', drag);
+    
+    svg.addEventListener('touchstart', startDrag);
+    svg.addEventListener('touchmove', drag);
+    
+
+    function getMousePosition(evt) {
+    var CTM = svg.getScreenCTM();
+    if (evt.touches) { evt = evt.touches[0]; }
+    return {
+        x: (evt.clientX - CTM.e) / CTM.a,
+        y: (evt.clientY - CTM.f) / CTM.d
+    };
+    }
+
+    var selectedElement, offset, transform;
+
+    function startDrag(evt) {
+    if (evt.target.classList.contains('draggable')) {
+        selectedElement = evt.target;
+        offset = getMousePosition(evt);
+
+        // Make sure the first transform on the element is a translate transform
+        var transforms = selectedElement.transform.baseVal;
+
+        if (transforms.length === 0 || transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE) {
+        // Create an transform that translates by (0, 0)
+        var translate = svg.createSVGTransform();
+        translate.setTranslate(0, 0);
+        selectedElement.transform.baseVal.insertItemBefore(translate, 0);
+        }
+
+        // Get initial translation
+        transform = transforms.getItem(0);
+        offset.x -= transform.matrix.e;
+        offset.y -= transform.matrix.f;
+    }
+    }
+
+    function drag(evt) {
+    if (selectedElement) {
+        evt.preventDefault();
+        var coord = getMousePosition(evt);
+        transform.setTranslate(coord.x - offset.x, coord.y - offset.y);
+    }
+    }
+
+    function endDrag(evt) {
+    selectedElement = false;
+    }
 }
