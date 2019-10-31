09f41d80655c12d72de0b959a1980a8a3834b075
Jordan McKenzie <46385517+jordanthejam24@users.noreply.github.com>
2019-10-31 14:31:53 -0700

    Updating to html5 canvas

--- get-squares/script.js
+++ get-squares/script.js
@@ -1,64 +1,64 @@
+//changing from svg to canvas completely. Waiting on research.
 
+// changeCursor = function(e){
+//     var x = e.clientX;
+//     var y = e.clientY;
+//     var coor = "Coordinates: (" + x + "," + y + ")";
+//     document.getElementById("demo").innerHTML = coor; 
+// }
 
-changeCursor = function(e){
-    var x = e.clientX;
-    var y = e.clientY;
-    var coor = "Coordinates: (" + x + "," + y + ")";
-    document.getElementById("demo").innerHTML = coor; 
-}
+// function makeDraggable(evt) {
+//     var svg = evt.target;
 
-function makeDraggable(evt) {
-    var svg = evt.target;
-
-    svg.addEventListener('mousedown', startDrag);
-    svg.addEventListener('mousemove', drag);
+//     svg.addEventListener('mousedown', startDrag);
+//     svg.addEventListener('mousemove', drag);
     
-    svg.addEventListener('touchstart', startDrag);
-    svg.addEventListener('touchmove', drag);
+//     svg.addEventListener('touchstart', startDrag);
+//     svg.addEventListener('touchmove', drag);
     
 
-    function getMousePosition(evt) {
-    var CTM = svg.getScreenCTM();
-    if (evt.touches) { evt = evt.touches[0]; }
-    return {
-        x: (evt.clientX - CTM.e) / CTM.a,
-        y: (evt.clientY - CTM.f) / CTM.d
-    };
-    }
+//     function getMousePosition(evt) {
+//     var CTM = svg.getScreenCTM();
+//     if (evt.touches) { evt = evt.touches[0]; }
+//     return {
+//         x: (evt.clientX - CTM.e) / CTM.a,
+//         y: (evt.clientY - CTM.f) / CTM.d
+//     };
+//     }
 
-    var selectedElement, offset, transform;
+//     var selectedElement, offset, transform;
 
-    function startDrag(evt) {
-    if (evt.target.classList.contains('draggable')) {
-        selectedElement = evt.target;
-        offset = getMousePosition(evt);
+//     function startDrag(evt) {
+//     if (evt.target.classList.contains('draggable')) {
+//         selectedElement = evt.target;
+//         offset = getMousePosition(evt);
 
-        // Make sure the first transform on the element is a translate transform
-        var transforms = selectedElement.transform.baseVal;
+//         // Make sure the first transform on the element is a translate transform
+//         var transforms = selectedElement.transform.baseVal;
 
-        if (transforms.length === 0 || transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE) {
-        // Create an transform that translates by (0, 0)
-        var translate = svg.createSVGTransform();
-        translate.setTranslate(0, 0);
-        selectedElement.transform.baseVal.insertItemBefore(translate, 0);
-        }
+//         if (transforms.length === 0 || transforms.getItem(0).type !== SVGTransform.SVG_TRANSFORM_TRANSLATE) {
+//         // Create an transform that translates by (0, 0)
+//         var translate = svg.createSVGTransform();
+//         translate.setTranslate(0, 0);
+//         selectedElement.transform.baseVal.insertItemBefore(translate, 0);
+//         }
 
-        // Get initial translation
-        transform = transforms.getItem(0);
-        offset.x -= transform.matrix.e;
-        offset.y -= transform.matrix.f;
-    }
-    }
+//         // Get initial translation
+//         transform = transforms.getItem(0);
+//         offset.x -= transform.matrix.e;
+//         offset.y -= transform.matrix.f;
+//     }
+//     }
 
-    function drag(evt) {
-    if (selectedElement) {
-        evt.preventDefault();
-        var coord = getMousePosition(evt);
-        transform.setTranslate(coord.x - offset.x, coord.y - offset.y);
-    }
-    }
+//     function drag(evt) {
+//     if (selectedElement) {
+//         evt.preventDefault();
+//         var coord = getMousePosition(evt);
+//         transform.setTranslate(coord.x - offset.x, coord.y - offset.y);
+//     }
+//     }
 
-    function endDrag(evt) {
-    selectedElement = false;
-    }
-}
+//     function endDrag(evt) {
+//     selectedElement = false;
+//     }
+// }
